08823e2893253212565de9078f5356c1514cc7f7
Dimitri Freitas <freitasdimitri7@gmail.com>
2022-08-19 23:36:39 -0300

    Teclado vinculado ao projeto agora é possivel digitar tbm pelo teclado

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ let calculo = document.querySelector('.calculo p');
 let resultado = document.querySelector('.calculo h1')
 
 
-// Digita o calculo
-function digitaCalculo() {
+// Digita o Numero
+function digitaNumero() {
   numeros.forEach((e) => {
     e.addEventListener('click', () => {
       calculo.innerHTML += e.innerHTML
     });
   });
 };
-digitaCalculo();
+digitaNumero();
 
 // Digita operação
 function digitaOperacao() {
@@ -42,7 +42,7 @@ function resultadoFinal() {
   if (resultadoConta) {
     resultado.innerHTML = eval(resultadoConta);
   } else {
-    document.getElementById('resultado').innerHTML = "Nada...";
+    resultado.innerHTML = 'Nada...'
   };
 
   calculo.innerHTML += ' =';
@@ -51,7 +51,6 @@ function resultadoFinal() {
 function porcentagem() {
   let teste = 0
   teste = calculo.innerHTML; 
-  console.log(teste);
   teste = `0.${teste} * `;
   calculo.innerHTML = teste
 }
@@ -62,3 +61,42 @@ function changeMode() {
   btn.classList.toggle('ativo');
   document.documentElement.classList.toggle('changeMode');
 }
+
+function teclado() {
+    document.documentElement.addEventListener('keydown', (e) => {
+    if(e.keyCode == 13) {
+      e.preventDefault();
+      resultadoFinal();
+    };
+    if(e.keyCode === 8) {
+      e.preventDefault();
+      apaga();
+    };
+    if(e.keyCode === 46) {
+      apagaTudo();
+    };
+    
+    // Numeros teclado + Numeros NumPad
+    if (e.keyCode >= 48 && e.keyCode <= 57 || e.keyCode >= 96 && e.keyCode <= 105){
+      calculo.innerHTML += e.key  
+    };
+
+    // Operadores teclado Numpad
+    if(e.keyCode === 110 || e.keyCode === 190) {
+      calculo.innerHTML += '.';
+    };
+    if(e.keyCode === 111 || e.keyCode === 193) {
+      calculo.innerHTML += ' / ';
+    };
+    if(e.keyCode === 107) {
+      calculo.innerHTML += ' + ';
+    };
+    if(e.keyCode === 109 || e.keyCode === 189) {
+      calculo.innerHTML += ' - ';
+    };
+    if(e.keyCode === 106) {
+      calculo.innerHTML += ' * ';
+    };
+  });
+};
+teclado();
